4b7a58d9609d37a1c550fe2c95cd6b9ba1ac58bb
gioina16 <gioinashvili16@gmail.com>
2025-07-04 20:19:07 +0400

    first commit

---
+++ level 002.py/homework.py
@@ -0,0 +1,28 @@
+print("shawarma")
+print("red")
+print("gaming")
+
+film01 = "Coco"
+film02 = "harry potter"
+film03 = "interstellar"
+film04 = "flash"
+film05 = "blacklist"
+
+print(film01)
+print(film02)
+print(film03)
+print(film04)
+print(film05)
+
+
+#კონკატინაცია ნიშნავს სტრინგების შეერთებას
+
+#string არის როცა სიტყვაში არის ბრჭყალები
+#integer არის მთელი რიცხვები
+#float არის ათწილადი რიცხვები
+
+
+name = "gio"
+age = 13
+
+print("მე" + "ვარ" + name + "და" + "ვარ" + str(age) +"წლის") 
