4f21579be1756e8de4cd6e060a36e46c155826d1
gioina16 <gioinashvili16@gmail.com>
2025-07-24 10:37:16 +0400

    first commit

---
+++ level 006/classwork.py
@@ -0,0 +1,16 @@
+name = "winter" 
+name2 = "park" 
+print(name+name2)
+
+
+#string
+name = "gio"
+print(name)
+
+
+#integer
+
+num = 10
+print(int(num))
+
+
---
+++ level 006/homework.py
@@ -0,0 +1,35 @@
+#boolean არის true and false და მისი ოპერატორებია <, >, ==, !=.
+
+print("gio")
+print("lomi")
+print("manqana")
+print("Banana")
+print("laptop")
+
+
+print(10)
+print(25)
+print(123)
+print(98)
+print(1)
+
+
+print(20.0)
+print(12.2)
+print(9.2)
+print(1.1)
+print(2.9)
+
+num=2
+num02 = 3
+print(num>num02)
+print(num<num02)
+print(num==num02)
+print(num02 != num)
+print( num <= num02)
+
+
+is_on = True
+is_off = False
+print('გამორთულია: ', is_off)
+print('ჩართულია : ', is_on)
